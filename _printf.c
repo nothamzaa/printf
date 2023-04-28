8940aad6698c737743732d0a19f45c7c44c60c31
hamza <hamzabousseta1@gmail.com>
2023-04-27 18:08:08 -0700

    one way on another

--- _printf.c
+++ _printf.c
@@ -1,48 +1,80 @@
 #include "main.h"
 
+// Declare function prototypes
+void print_buffer(char buffer[], int* buff_ind);
+
 /**
- * _printf - produces output according to a format.
- * @format: character string.
- * Return: the number of characters printed (excluding the null byte used to
- * end output to strings)
+ * _printf - Custom printf function that supports formatting options
+ * @format: A string containing the format specifier and optional arguments
+ * @...: Optional arguments to be printed, corresponding to the format specifier
+ * Return: The total number of characters printed
  */
-int _printf(const char *format, ...)
+int _printf(const char* format, ...)
 {
-        va_list args;
-        int i = 0, printed_chars = 0;
+    // Initialize variables and buffer
+    int printed_chars = 0, buff_ind = 0;
+    char buffer[BUFF_SIZE] = { 0 };
 
-        if (!format)
-                return (-1);
+    // Check if format is NULL
+    if (format == NULL)
+        return -1;
 
-        va_start(args, format);
+    // Initialize argument list
+    va_list arg_list;
+    va_start(arg_list, format);
 
-        while (format[i])
+    // Loop through each character in format
+    for (int i = 0; format[i] != '\0'; i++)
+    {
+        // Check if current character is a format specifier
+        if (format[i] == '%')
         {
-                if (format[i] == '%')
-                {
-                        i++;
-                        if (format[i] == '\0')
-                                return (-1);
-                        while (format[i] == ' ')
-                                i++;
-                        if (format[i] == '%')
-                                printed_chars += print_percent(args);
-                        else if (format[i] == 'c')
-                                printed_chars += print_char(args);
-                        else if (format[i] == 's')
-                                printed_chars += print_string(args);
-                        else if (format[i] == 'd' || format[i] == 'i')
-                                printed_chars += print_int(args);
-                        else
-                        {
-                                printed_chars += _putchar('%');
-                                printed_chars += _putchar(format[i]);
-                        }
-                }
-                else
-                        printed_chars += _putchar(format[i]);
-                i++;
+            // Print the contents of the buffer
+            print_buffer(buffer, &buff_ind);
+
+            // Parse the format specifier and print the corresponding value
+            int printed = handle_print(format, &i, arg_list, buffer);
+            if (printed == -1)
+            {
+                va_end(arg_list);
+                return -1;
+            }
+
+            // Update the total number of printed characters
+            printed_chars += printed;
         }
-        va_end(args);
-        return (printed_chars);
+        else
+        {
+            // Add the current character to the buffer
+            buffer[buff_ind++] = format[i];
+
+            // If the buffer is full, print its contents
+            if (buff_ind == BUFF_SIZE)
+                print_buffer(buffer, &buff_ind);
+
+            // Update the total number of printed characters
+            printed_chars++;
+        }
+    }
+
+    // Print any remaining characters in the buffer
+    print_buffer(buffer, &buff_ind);
+
+    // Clean up and return the total number of printed characters
+    va_end(arg_list);
+    return printed_chars;
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it is not empty
+ * @buffer: An array of characters representing the buffer
+ * @buff_ind: A pointer to the index of the next empty position in the buffer
+ */
+void print_buffer(char buffer[], int* buff_ind)
+{
+    if (*buff_ind > 0)
+        write(1, buffer, *buff_ind);
+
+    // Reset the buffer index to zero
+    *buff_ind = 0;
 }
--- main.h
+++ main.h
@@ -2,12 +2,63 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int _putchar(char c);
+// Define the buffer size to be used for printing
+#define BUFF_SIZE 1024
+
+// Define various flags to be used for formatting
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+// Define sizes for integer data types
+#define S_LONG 2
+#define S_SHORT 1
+
+// Define a struct for holding information about the format and associated function
+struct fmt {
+    char fmt;
+    int (*fn)(va_list, char[], int, int, int, int);
+};
+
+// Define a type for the above struct
+typedef struct fmt fmt_t;
+
+// Define a function for printing formatted output
 int _printf(const char *format, ...);
-int print_char(va_list args);
-int print_string(va_list args);
-int print_percent(va_list args);
-int print_int(va_list args);
 
-#endif /* MAIN_H */
+// Define a function for handling printing based on the format string
+int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
+
+// Define various functions for printing different data types and formats
+int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_int(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[], char buffer[], int flags, char flag_ch, int width, int precision, int size);
+int print_non_printable(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_pointer(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_reverse(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_rot13string(va_list types, char buffer[], int flags, int width, int precision, int size);
+
+// Define various functions for handling format string parsing
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+
+// Define various functions for writing output to the buffer
+int handle_write_char(char c, char buffer[], int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[], int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision, int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length, int width, int flags, char padd, char extra_c, int padd_start);
+int write_unsgnd(int is_negative, int
