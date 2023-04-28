7d863c4eaf8079ad73df310676ee392e4444edf4
hamza <hamzabousseta1@gmail.com>
2023-04-27 17:33:51 -0700

    fix 1

--- _printf.c
+++ _printf.c
@@ -1,87 +1,141 @@
 #include <stdarg.h>
 #include <unistd.h>
+#include "main.h"
 
 /**
- * _printf - prints a formatted string to standard output
- * @format: format string containing directives
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: number of characters printed
+ * Return: On success 1.
+ * On error, -1 is returned and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _print_char - prints a character
+ * @arg: list containing a single character to print
+ *
+ * Return: the number of characters printed
+ */
+int _print_char(va_list arg)
+{
+	char c = va_arg(arg, int);
+	_putchar(c);
+	return (1);
+}
+
+/**
+ * _print_string - prints a string
+ * @arg: list containing a string to print
+ *
+ * Return: the number of characters printed
+ */
+int _print_string(va_list arg)
+{
+	char *str = va_arg(arg, char *);
+	int count = 0;
+
+	if (str == NULL)
+		str = "(null)";
+	while (*str)
+	{
+		_putchar(*str);
+		count++;
+		str++;
+	}
+	return (count);
+}
+
+/**
+ * _print_percent - prints a percent sign
+ * @arg: unused
+ *
+ * Return: the number of characters printed
+ */
+int _print_percent(__attribute__((unused)) va_list arg)
+{
+	_putchar('%');
+	return (1);
+}
+
+/**
+ * _print_binary - prints an unsigned int in binary form
+ * @arg: list containing an unsigned int to print
+ *
+ * Return: the number of characters printed
+ */
+int _print_binary(va_list arg)
+{
+	unsigned int num = va_arg(arg, unsigned int);
+	int count = 0;
+
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	else if (num == 1)
+	{
+		_putchar('1');
+		return (1);
+	}
+
+	while (num)
+	{
+		_putchar((num & 1) + '0');
+		num >>= 1;
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * _printf - prints output according to a format
+ * @format: list of zero or more directives
+ *
+ * Return: the number of characters printed
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    char c;
-    char *str;
-    int count = 0, num;
-    int is_negative = 0;
+	va_list arg;
+	int count = 0;
 
-    va_start(args, format);
-
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            switch (*format)
-            {
-                case 'c':
-                    c = va_arg(args, int);
-                    write(1, &c, 1);
-                    count++;
-                    break;
-                case 's':
-                    str = va_arg(args, char *);
-                    while (*str)
-                    {
-                        write(1, str, 1);
-                        str++;
-                        count++;
-                    }
-                    break;
-                case 'd':
-                case 'i':
-                    num = va_arg(args, int);
-                    if (num < 0)
-                    {
-                        is_negative = 1;
-                        num = -num;
-                    }
-                    if (num == 0)
-                    {
-                        write(1, "0", 1);
-                        count++;
-                    }
-                    while (num > 0)
-                    {
-                        char digit = (num % 10) + '0';
-                        write(1, &digit, 1);
-                        count++;
-                        num /= 10;
-                    }
-                    if (is_negative)
-                    {
-                        write(1, "-", 1);
-                        count++;
-                        is_negative = 0;
-                    }
-                    break;
-                case '%':
-                    write(1, "%", 1);
-                    count++;
-                    break;
-                default:
-                    break;
-            }
-        }
-        else
-        {
-            write(1, format, 1);
-            count++;
-        }
-        format++;
-    }
-
-    va_end(args);
-
-    return count;
+	va_start(arg, format);
+	while (format && *format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+			case 'c':
+				count += _print_char(arg);
+				break;
+			case 's':
+				count += _print_string(arg);
+				break;
+			case '%':
+				count += _print_percent(arg);
+				break;
+			case 'b':
+				count += _print_binary(arg);
+				break;
+			default:
+				_putchar('%');
+				_putchar(*format);
+				count += 2;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(arg);
+	return (count);
 }
